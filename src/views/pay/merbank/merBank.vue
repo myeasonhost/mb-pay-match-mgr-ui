15ac10ddc7aa6d498560515421f5d224208d5b39
willie <45468@qq.com>
2022-09-07 22:28:54 +0800

    银行卡管理1

--- src/views/pay/merbank/merBank.vue
+++ src/views/pay/merbank/merBank.vue
@@ -60,58 +60,60 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['pay:merBank:add']"
-        >新增</el-button>
+        >新增
+        </el-button>
       </el-col>
-<!--      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['pay:merBank:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['pay:merBank:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['pay:merBank:export']"
-        >导出</el-button>
-      </el-col>
-      -->
+      <!--      <el-col :span="1.5">
+              <el-button
+                type="success"
+                plain
+                icon="el-icon-edit"
+                size="mini"
+                :disabled="single"
+                @click="handleUpdate"
+                v-hasPermi="['pay:merBank:edit']"
+              >修改</el-button>
+            </el-col>
+            <el-col :span="1.5">
+              <el-button
+                type="danger"
+                plain
+                icon="el-icon-delete"
+                size="mini"
+                :disabled="multiple"
+                @click="handleDelete"
+                v-hasPermi="['pay:merBank:remove']"
+              >删除</el-button>
+            </el-col>
+            <el-col :span="1.5">
+              <el-button
+                type="warning"
+                plain
+                icon="el-icon-download"
+                size="mini"
+                @click="handleExport"
+                v-hasPermi="['pay:merBank:export']"
+              >导出</el-button>
+            </el-col>
+            -->
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="bankList" @selection-change="handleSelectionChange">
-      <el-table-column label="银行卡名称" align="center" prop="bankName" />
-      <el-table-column label="姓名" align="center" prop="userName" />
-      <el-table-column label="银行账户" align="center" prop="bankNum" />
-      <el-table-column label="分行名称" align="center" prop="branchName" />
-      <el-table-column label="银行编码" align="center" prop="bankCode" />
-      <el-table-column label="单笔限额" align="center" prop="singleLimit" />
-      <el-table-column label="每日限额" align="center" prop="dayLimit" />
+      <el-table-column label="银行卡名称" align="center" prop="bankName"/>
+      <el-table-column label="姓名" align="center" prop="userName"/>
+      <el-table-column label="银行账户" align="center" prop="bankNum"/>
+      <el-table-column label="分行名称" align="center" prop="branchName"/>
+      <el-table-column label="银行编码" align="center" prop="bankCode"/>
+      <el-table-column label="单笔限额" align="center" prop="singleLimit"/>
+      <el-table-column label="每日限额" align="center" prop="dayLimit"/>
       <el-table-column label="状态" align="center" prop="status">
         <template slot-scope="scope">
           <dict-tag :options="dict.type.sys_bank_status" :value="scope.row.status"/>
         </template>
       </el-table-column>
-      <el-table-column label="创建时间" align="center" prop="createTime" sortable="custom" :sort-orders="['descending', 'ascending']" width="180">
+      <el-table-column label="创建时间" align="center" prop="createTime" sortable="custom"
+                       :sort-orders="['descending', 'ascending']" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
         </template>
@@ -124,14 +126,16 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['pay:merBank:edit']"
-          >修改</el-button>
+          >修改
+          </el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['pay:merBank:remove']"
-          >删除</el-button>
+          >删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -148,25 +152,25 @@
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="银行卡名称" prop="bankName">
-          <el-input v-model="form.bankName" placeholder="请输入银行卡名称" />
+          <el-input v-model="form.bankName" placeholder="请输入银行卡名称"/>
         </el-form-item>
         <el-form-item label="银行账户" prop="bankNum">
-          <el-input v-model="form.bankNum" placeholder="请输入银行账户" />
+          <el-input v-model="form.bankNum" placeholder="请输入银行账户"/>
         </el-form-item>
         <el-form-item label="分行名称" prop="branchName">
-          <el-input v-model="form.branchName" placeholder="请输入支行名称" />
+          <el-input v-model="form.branchName" placeholder="请输入支行名称"/>
         </el-form-item>
         <el-form-item label="姓名" prop="userName">
-          <el-input v-model="form.userName" placeholder="请输入姓名" />
+          <el-input v-model="form.userName" placeholder="请输入姓名"/>
         </el-form-item>
         <el-form-item label="银行编码" prop="bankCode">
-          <el-input v-model="form.bankCode" placeholder="请输入银行编码" />
+          <el-input v-model="form.bankCode" placeholder="请输入银行编码"/>
         </el-form-item>
         <el-form-item label="单笔限额" prop="singleLimit">
-          <el-input v-model="form.singleLimit" placeholder="请输入单笔限额" />
+          <el-input v-model="form.singleLimit" placeholder="请输入单笔限额"/>
         </el-form-item>
         <el-form-item label="每日限额" prop="dayLimit">
-          <el-input v-model="form.dayLimit" placeholder="请输入每日限额" />
+          <el-input v-model="form.dayLimit" placeholder="请输入每日限额"/>
         </el-form-item>
         <el-form-item label="状态" prop="status">
           <el-select
@@ -182,9 +186,9 @@
             />
           </el-select>
         </el-form-item>
-<!--        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" placeholder="请输入备注" />
-        </el-form-item>-->
+        <!--        <el-form-item label="备注" prop="remark">
+                  <el-input v-model="form.remark" placeholder="请输入备注" />
+                </el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -195,195 +199,221 @@
 </template>
 
 <script>
-import { listBank, getBank, delBank, addBank, updateBank, exportBank } from "@/api/pay/bank";
-import { getUserProfile } from "@/api/system/user";
+  import {listBank, getBank, delBank, addBank, updateBank, exportBank} from "@/api/pay/bank";
+  import {getUserProfile} from "@/api/system/user";
 
-export default {
-  name: "Bank",
-  dicts: ['sys_currency','sys_bank_status'],
-  components: {
-  },
-  data() {
-    return {
-      // 遮罩层
-      loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
-      // 显示搜索条件
-      showSearch: true,
-      // 总条数
-      total: 0,
-      // 收款银行表格数据
-      bankList: [],
-      // 日期范围
-      dateRange: [],
-      //登录用户信息
-      user: {},
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
-      // 查询参数
-      queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        siteId:this.$store.state.user.name,
-        bankName: undefined,
-        bankNum: undefined,
-        branchName: undefined,
-        userName: undefined,
-        bankCode: undefined,
-        currency: undefined,
-        dayLimit: undefined,
-        monthLimit: undefined,
-        singleLimit: undefined,
-        dayReceiptAmount: undefined,
-        totalReceiptAmount: undefined,
-        operCode: undefined,
-        status: undefined,
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        bankName: [
-          { required: true, message: "银行卡名称不能为空", trigger: "blur" }
-        ],
-        bankNum: [
-          { required: true, message: "银行账户不能为空", trigger: "blur" }
-        ],
-        userName: [
-          { required: true, message: "姓名不能为空", trigger: "blur" }
-        ]
+  export default {
+    name: "Bank",
+    dicts: ['sys_currency', 'sys_bank_status'],
+    components: {},
+    data() {
+      const checkAmount = (rule, value, callback) => {
+        if (value) {
+          var reg = /^[0-9]{0,16}$/
+          if (reg.test(value) === false) {
+            callback(new Error("只能输入16位正整数"))
+          }else{
+            callback()
+          }
+        } else {
+          callback()
+        }
       }
-    };
-  },
-  created() {
-    this.getList();
-  },
-  methods: {
-    /** 查询收款银行列表 */
-    getList() {
-      this.loading = true;
-      listBank(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
-        this.bankList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        id: undefined,
-        siteId:this.$store.state.user.name,
-        bankName: undefined,
-        bankNum: undefined,
-        branchName: undefined,
-        userName: undefined,
-        bankCode: undefined,
-        currency: undefined,
-        dayLimit: undefined,
-        monthLimit: undefined,
-        singleLimit: undefined,
-        dayReceiptAmount: undefined,
-        totalReceiptAmount: undefined,
-        operCode: this.$store.state.user.name,
-        status: "1",
-        remark: undefined,
-        createTime: undefined,
-        updateTime: undefined
+      return {
+        // 遮罩层
+        loading: true,
+        // 选中数组
+        ids: [],
+        // 非单个禁用
+        single: true,
+        // 非多个禁用
+        multiple: true,
+        // 显示搜索条件
+        showSearch: true,
+        // 总条数
+        total: 0,
+        // 收款银行表格数据
+        bankList: [],
+        // 日期范围
+        dateRange: [],
+        //登录用户信息
+        user: {},
+        // 弹出层标题
+        title: "",
+        // 是否显示弹出层
+        open: false,
+        // 查询参数
+        queryParams: {
+          pageNum: 1,
+          pageSize: 10,
+          siteId: this.$store.state.user.name,
+          bankName: undefined,
+          bankNum: undefined,
+          branchName: undefined,
+          userName: undefined,
+          bankCode: undefined,
+          currency: undefined,
+          dayLimit: undefined,
+          monthLimit: undefined,
+          singleLimit: undefined,
+          dayReceiptAmount: undefined,
+          totalReceiptAmount: undefined,
+          operCode: undefined,
+          status: undefined,
+        },
+        // 表单参数
+        form: {},
+        // 表单校验
+        rules: {
+          bankName: [
+            {required: true, message: "请输入银行卡名称", trigger: "blur"},
+            {min: 2, max: 100, message: "长度2-100个字符", trigger: "blur"},
+          ],
+          branchName: [
+            {min: 2, max: 100, message: "长度2-100个字符", trigger: "blur"}
+          ],
+          bankNum: [
+            {required: true, message: "请输入银行账户", trigger: "blur"},
+            {min: 1, max: 20, message: "长度1-20个字符", trigger: "blur"},
+          ],
+          userName: [
+            {required: true, message: "请输入姓名", trigger: "blur"},
+            {min: 1, max: 50, message: "长度1-50个字符", trigger: "blur"},
+          ],
+          bankCode: [
+            {min: 0, max: 20, message: "长度0-20个字符", trigger: "blur"},
+          ],
+          dayLimit: [
+            {trigger: "blur", validator: checkAmount}//自定义规则
+          ],
+          singleLimit: [
+            {trigger: "blur", validator: checkAmount}//自定义规则
+          ]
+        }
       };
-      this.resetForm("form");
     },
-    /** 搜索按钮操作 */
-    handleQuery() {
-      this.queryParams.pageNum = 1;
+    created() {
       this.getList();
     },
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
-    },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加收款银行";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const id = row.id || this.ids
-      getBank(id).then(response => {
-        this.form = response.data;
+    methods: {
+      /** 查询收款银行列表 */
+      getList() {
+        this.loading = true;
+        listBank(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+          this.bankList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        });
+      },
+      // 取消按钮
+      cancel() {
+        this.open = false;
+        this.reset();
+      },
+      // 表单重置
+      reset() {
+        this.form = {
+          id: undefined,
+          siteId: this.$store.state.user.name,
+          bankName: undefined,
+          bankNum: undefined,
+          branchName: undefined,
+          userName: undefined,
+          bankCode: undefined,
+          currency: undefined,
+          dayLimit: undefined,
+          monthLimit: undefined,
+          singleLimit: undefined,
+          dayReceiptAmount: undefined,
+          totalReceiptAmount: undefined,
+          operCode: this.$store.state.user.name,
+          status: "1",
+          remark: undefined,
+          createTime: undefined,
+          updateTime: undefined
+        };
+        this.resetForm("form");
+      },
+      /** 搜索按钮操作 */
+      handleQuery() {
+        this.queryParams.pageNum = 1;
+        this.getList();
+      },
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("queryForm");
+        this.handleQuery();
+      },
+      // 多选框选中数据
+      handleSelectionChange(selection) {
+        this.ids = selection.map(item => item.id)
+        this.single = selection.length !== 1
+        this.multiple = !selection.length
+      },
+      /** 新增按钮操作 */
+      handleAdd() {
+        this.reset();
         this.open = true;
-        this.title = "修改收款银行";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.id != null) {
-            updateBank(this.form).then(response => {
-              this.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            this.form
-            addBank(this.form).then(response => {
-              this.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
+        this.title = "添加收款银行";
+      },
+      /** 修改按钮操作 */
+      handleUpdate(row) {
+        this.reset();
+        const id = row.id || this.ids
+        getBank(id).then(response => {
+          this.form = response.data;
+          this.open = true;
+          this.title = "修改收款银行";
+        });
+      },
+      /** 提交按钮 */
+      submitForm() {
+        this.$refs["form"].validate(valid => {
+          if (valid) {
+            if (this.form.id != null) {
+              updateBank(this.form).then(response => {
+                this.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              });
+            } else {
+              this.form
+              addBank(this.form).then(response => {
+                this.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              });
+            }
           }
-        }
-      });
-    },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const ids = row.id || this.ids;
-      this.$confirm('是否确认删除收款银行编号为"' + ids + '"的数据项?', "警告", {
+        });
+      },
+      /** 删除按钮操作 */
+      handleDelete(row) {
+        const ids = row.id || this.ids;
+        this.$confirm('是否确认删除收款银行编号为"' + ids + '"的数据项?', "警告", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
           type: "warning"
-        }).then(function() {
+        }).then(function () {
           return delBank(ids);
         }).then(() => {
           this.getList();
           this.msgSuccess("删除成功");
         })
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      const queryParams = this.queryParams;
-      this.$confirm('是否确认导出所有收款银行数据项?', "警告", {
+      },
+      /** 导出按钮操作 */
+      handleExport() {
+        const queryParams = this.queryParams;
+        this.$confirm('是否确认导出所有收款银行数据项?', "警告", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
           type: "warning"
-        }).then(function() {
+        }).then(function () {
           return exportBank(queryParams);
         }).then(response => {
           this.download(response.msg);
         })
+      }
     }
-  }
-};
+  };
 </script>
